d77581e70c46f512750ecf7d6bc58464285c5e59
Michael Fleming <mgfleming@verisk.com>
2021-08-19 20:03:25 -0600

    main: added EssentialFeed framework project and FeedItem file

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Michael Fleming on 8/19/21.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
