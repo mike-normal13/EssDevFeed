020ff102e317cbc4cb90d15aedfd206077e52875
Michael Fleming <mgfleming@verisk.com>
2021-08-23 18:31:33 -0600

    main: RemoteFeedLoader does not request data upon creation

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Michael Fleming on 8/21/21.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+
+}
