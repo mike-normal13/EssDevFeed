9a698584548ec1d3cecfc572aa6a8b73e7347cd2
Michael Fleming <mgfleming@verisk.com>
2021-08-24 19:03:14 -0600

    main: Guarantee that we can load more than once

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -15,13 +15,12 @@ public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
    
-   
     public init(url: URL, client: HTTPClient) {
        self.url = url
        self.client = client
-   }
+    }
    
-   public func load() {
-       client.get(from: url)
-   }
+    public func load() {
+        client.get(from: url)
+    }
 }
